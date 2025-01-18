2b53d5358000be83f1674c63ae8be7564fddac2d
sailesh123kumar <saileshkumar17893@gmail.com>
2025-01-18 11:33:29 +0530

    modified folder structure

--- src/main/java/ArraysProgram/CommonElementInTwoArray.java
+++ src/main/java/ArraysProgram/CommonElementInTwoArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class CommonlementInTwoArray {
+public class CommonElementInTwoArray {
 	
 	
 	public static void main(String[] args) {
